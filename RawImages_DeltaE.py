e8ba370afe6ad1b2ebfdd40f028cc9bc5ebd28b6
jrafshoon-nelumbo <j.rafshoon@nelumbo.io>
2026-08-21 16:14:15 -0700

    Added in ROI  selection tool

--- RawImages_DeltaE.py
+++ RawImages_DeltaE.py
@@ -1,21 +1,24 @@
 import numpy as np
 from skimage import color
 import rawpy
+import cv2
 
 
-def raw_to_delta_e(raw_path1, raw_path2, roi1, roi2):
-    """
-    Calculate Delta E between two RAW images in specified ROIs.
+def raw_to_delta_e(raw_path1, raw_path2):
     
-    Parameters:
-        raw_path1 (str): Path to the first RAW image.
-        raw_path2 (str): Path to the second RAW image.
-        roi1 (tuple): Region of interest for the first image (x1, y1, x2, y2).
-        roi2 (tuple): Region of interest for the second image (x1, y1, x2, y2).
+    roi_lib = {}
+    for img_path in [raw_path1, raw_path2]:
+        img = cv2.imread(img_path)
     
-    Returns:
-        float: Calculated Delta E value.
-    """
+        box = cv2.selectROI("Select Fabric Region", img, showCrosshair=True) #open a window to select the ROI, SPACE or ENTER to confirm, 'c' to cancel
+        cv2.destroyAllWindows()
+
+        x, y, w, h = box #returns the coordinates of the selected ROI as (x, y, width, height)
+        y1, y2 = y, y + h
+        x1, x2 = x, x + w
+
+        roi_lib[img_path] = (y1, y2, x1, x2)
+    
     with rawpy.imread(raw_path1) as raw1, rawpy.imread(raw_path2) as raw2:
         rgb_linear1 = raw1.postprocess(gamma=(1, 1), no_auto_bright=True, output_bps=16)
         rgb_linear2 = raw2.postprocess(gamma=(1, 1), no_auto_bright=True, output_bps=16)
@@ -26,20 +29,17 @@ def raw_to_delta_e(raw_path1, raw_path2, roi1, roi2):
         lab_image1 = color.rgb2lab(rgb_normalized1)
         lab_image2 = color.rgb2lab(rgb_normalized2)
         
-        lab_roi1 = np.mean(lab_image1[roi1[1]:roi1[3], roi1[0]:roi1[2]], axis=(0, 1))
-        lab_roi2 = np.mean(lab_image2[roi2[1]:roi2[3], roi2[0]:roi2[2]], axis=(0, 1))
+        lab_roi1 = np.mean(lab_image1[roi_lib[raw_path1][1]:roi_lib[raw_path1][3], roi_lib[raw_path1][0]:roi_lib[raw_path1][2]], axis=(0, 1))
+        lab_roi2 = np.mean(lab_image2[roi_lib[raw_path2][1]:roi_lib[raw_path2][3], roi_lib[raw_path2][0]:roi_lib[raw_path2][2]], axis=(0, 1))
         
         delta_e = np.linalg.norm(lab_roi1 - lab_roi2)
         
     return delta_e
-
+#### Add in AATCC scoring for color change
 
 if __name__ == "__main__":
-    # Example usage
     raw_path1 = "BlueTS_Raw.dng"
     raw_path2 = "GrayTS_Raw.dng"
-    roi1 = (100, 200, 100, 200)  # ROI for the first image
-    roi2 = (100, 200, 100, 200)  # ROI for the second image
-    
-    delta_e_value = raw_to_delta_e(raw_path1, raw_path2, roi1, roi2)
-    print(f"Calculated Delta E: {delta_e_value:.2f}")
+        
+    delta_e_value = raw_to_delta_e(raw_path1, raw_path2)
+    print(f"Calculated Delta E: {delta_e_value:.3f}") #print the Delta E value with 3 decimal places for better precision 
